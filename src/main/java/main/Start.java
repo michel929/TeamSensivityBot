ad50605f04630e0faf69c6be156fd95d5a325e47
michel929 <michel.gruenig@gmail.com>
2022-12-19 08:04:04 +0100

    Update Start.java

--- src/main/java/main/Start.java
+++ src/main/java/main/Start.java
@@ -10,7 +10,6 @@ import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.OnlineStatus;
 import net.dv8tion.jda.api.entities.Activity;
-import net.dv8tion.jda.api.interactions.commands.Command;
 import net.dv8tion.jda.api.interactions.commands.OptionType;
 import net.dv8tion.jda.api.interactions.commands.build.SubcommandData;
 import net.dv8tion.jda.api.requests.GatewayIntent;
